7befcecc771c918b5047254e7ccb040c63e46445
Bruno Gonçalo <brplays21@gmail.com>
2026-06-18 23:49:30 +0100

    fix: hardcode api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://barber-api-v1.onrender.com',
 })
 
 // Injeta o token automaticamente em todos os pedidos
